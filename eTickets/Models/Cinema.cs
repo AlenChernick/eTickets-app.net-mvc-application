3f75559262aec7541da6ee93ce5c0a4a17a06025
AlenChernick <crazyguy771@gmail.com>
2022-10-01 00:17:45 +0300

    Added model realtionships

--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace eTickets.Models
 {
@@ -9,5 +10,8 @@ namespace eTickets.Models
         public string Logo { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        //Realtionships
+        public List<Movie> Movies { get; set; }
     }
 }
