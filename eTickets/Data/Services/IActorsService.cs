b96338df8a874f281f6bff1aed34e0be8d1b269c
AlenChernick <crazyguy771@gmail.com>
2022-10-01 20:36:02 +0300

    Added  buton - Add New + preview of picture

--- eTickets/Data/Services/IActorsService.cs
+++ eTickets/Data/Services/IActorsService.cs
@@ -1,12 +1,13 @@
 ﻿using eTickets.Models;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace eTickets.Data.Services
 {
-    public interface IActorService
+    public interface IActorsService
     {
-        IEnumerable<Actor> GetAll();
+        Task<IEnumerable<Actor>> GetAll();
         Actor GetById(int id);
         void Add(Actor actor);
         Actor Update(int id, Actor newActor);
